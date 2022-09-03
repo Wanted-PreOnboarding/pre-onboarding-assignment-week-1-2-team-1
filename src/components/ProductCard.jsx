ed73b71917065b2fb5c34efc90f7d0ed823da4ef
unsnruu <unsnruu@gmail.com>
2022-09-03 15:38:57 +0900

    [#4] style : 기본적인 스타일 더함
    컴포넌트의 기본적인 스타일을 수정하고, 주석을 달았습니다.
    이후 리팩토링 과정에서 제거할 예정입니다.

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,18 +1,49 @@
+import styled from '@emotion/styled';
+
+//todo 리팩토링 과정을 통해서 style로 컴포넌트 옮기기
+
+const ProductCardContainer = styled.div`
+  background-color: red;
+  width: 240px;
+  height: 334px;
+  display: flex;
+  flex-direction: column;
+
+  & img {
+    width: 100%;
+  }
+  & h3 {
+    font-size: 14px;
+  }
+  .card-description {
+  }
+`;
+
 function ProductCard({ id, img, isSale, name, price, salePrice }) {
+  const discountPercentage = (100 - (salePrice / price) * 100).toFixed(0);
+
   return (
-    <div>
+    <ProductCardContainer>
       <div>
-        {/* 이후에 absolute로 sale, best,md를 추가하기 위해서 div로 감쌌음*/}
+        {/* 이후에 absolute로 sale, best,md badge를 추가하기 위해서 div로 감쌌음*/}
         <img src={img} alt="" />
       </div>
       <div className="card-description">
         <div>
           <h3>{name}</h3>
-          <span>{price}</span>
-          <span>{salePrice}</span>
+          <div>
+            {/* discount and sale price */}
+            <span>{discountPercentage}%</span>
+            <span>{salePrice}</span>
+          </div>
+          <div>
+            {/* price and delivery fee */}
+            <span>{price}</span>
+            <span>무료배송</span>
+          </div>
         </div>
       </div>
-    </div>
+    </ProductCardContainer>
   );
 }
 
