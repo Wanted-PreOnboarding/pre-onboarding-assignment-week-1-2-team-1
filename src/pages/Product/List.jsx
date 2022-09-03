b593562b20d64decfac6122426e5280215f5a3bc
unsnruu <unsnruu@gmail.com>
2022-09-03 23:13:48 +0900

    [#4] style : Introduction에 스타일 추가
    Introduction styled component에 스타일을 추가하였습니다.
    부수적으로 내부적인 컨텐츠를 조금 추가하였습니다.

--- src/pages/Product/List.jsx
+++ src/pages/Product/List.jsx
@@ -6,20 +6,49 @@ import ProductCard from '../../components/ProductCard';
 
 //todo: refacotring을 할 때, style 폴더로 옮겨야 함.
 const ListContainer = styled.div`
+  width: 100%;
   display: flex;
   justify-content: center;
   flex-wrap: wrap;
 `;
-const Introduction = styled.div`
+const Introduction = styled.section`
   width: 100%;
   height: 313px;
   background-color: #eef8ec;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  & img {
+    width: 46px;
+    height: 46px;
+  }
+  & h3 {
+    color: #4a9536;
+    font-size: 22px;
+    font-weight: bold;
+  }
+  & p {
+    text-align: center;
+    line-height: 19px;
+    display: flex;
+    flex-direction: column;
+  }
+  & p span {
+    margin-bottom: 10px;
+  }
 `;
-const ProductCardContainer = styled.div`
+const ChipMenu = styled.section`
+  width: 100%;
+  height: 160px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+const ProductCardContainer = styled.section`
   display: flex;
   justify-content: center;
   flex-wrap: wrap;
-
   & > div {
     margin-right: 10px;
     margin-bottom: 28px;
@@ -28,6 +57,9 @@ const ProductCardContainer = styled.div`
 
 function List() {
   const [productItems, setproductItems] = useState([]);
+  //todo pagination state로 관리 해야함.
+
+  const chipsMenus = ['Sale', 'Best', 'MD', '전체'];
 
   useEffect(() => {
     const fetchProducItems = async () => {
@@ -42,31 +74,33 @@ function List() {
   return (
     <ListContainer>
       <Introduction>
+        <img src="/imgs/프룻파비콘.png" alt="프롯 로고" />
+        <h3>프루떼 [수확배송]</h3>
         <p>
-          친환경 농가의 맛있고 바른 먹거리를 만났을때 게릴라로 열리는 프루떼의 반짝스토어 가장
-          알맞게 익었을때 농장에서 바로! 수확해서 배송해드립니다.
+          <span>
+            친환경 농가의 맛있고 바른 먹거리를 만났을때 게릴라로 열리는 프루떼의 반짝스토어
+          </span>
+          <span>가장 알맞게 익었을때 농장에서 바로! 수확해서 배송해드립니다.</span>
         </p>
       </Introduction>
-      <div>
-        <ul>
-          {
-            //메뉴 나열
-          }
-        </ul>
-        <ProductCardContainer>
-          {productItems.map(({ id, img, isSale, name, price, salePrice }) => (
-            <ProductCard
-              key={id}
-              id={id}
-              img={img}
-              isSale={isSale}
-              name={name}
-              price={price}
-              salePrice={salePrice}
-            />
-          ))}
-        </ProductCardContainer>
-      </div>
+      <ChipMenu>
+        {chipsMenus.map((chipMenu, idx) => (
+          <span key={idx}>{chipMenu}</span>
+        ))}
+      </ChipMenu>
+      <ProductCardContainer>
+        {productItems.map(({ id, img, isSale, name, price, salePrice, chip }) => (
+          <ProductCard
+            key={id}
+            id={id}
+            img={img}
+            isSale={isSale}
+            name={name}
+            price={price}
+            salePrice={salePrice}
+          />
+        ))}
+      </ProductCardContainer>
     </ListContainer>
   );
 }
