5fa6f9cc687e70a8e83d35e06237300e8b6ee4cf
unsnruu <unsnruu@gmail.com>
2022-09-03 23:57:49 +0900

    [#4] feat : Link를 통해 상세 페이지를 이동하는 기능 구현
    react-router-dom에서 제공하는 Link component를 통해서
    상세페이지로 이동하는 기능을 구현하였습니다.

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
 
 //todo: 리팩토링 과정을 통해서 style로 컴포넌트 옮기기
 const ProductCardContainer = styled.div`
@@ -6,6 +7,7 @@ const ProductCardContainer = styled.div`
   height: 334px;
   display: flex;
   flex-direction: column;
+  text-decoration: none;
   & img {
     width: 240px;
     height: 240px;
@@ -18,11 +20,16 @@ const ProductCardDescription = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  & > h3 {
+  & > a {
+    text-decoration: none;
+  }
+  & > a > h3 {
     font-size: 14px;
     margin: 0;
     margin-top: 12px;
     height: 32px;
+
+    color: #000000;
   }
   & > div {
     margin-bottom: 5px;
@@ -53,15 +60,18 @@ const PriceAndDelivery = styled.div`
 
 function ProductCard({ id, img, isSale, name, price, salePrice, chip }) {
   const discountPercentage = (100 - (salePrice / price) * 100).toFixed(0);
+  const url = `/fruit/${id}`;
 
   return (
     <ProductCardContainer>
-      <div>
+      <Link to={url}>
         {/* todo: 이후에 absolute로 sale, best,md badge를 추가하기 위해서 div로 감쌌음 */}
         <img src={img} alt={name} />
-      </div>
+      </Link>
       <ProductCardDescription>
-        <h3>{name}</h3>
+        <Link to={url}>
+          <h3>{name}</h3>
+        </Link>
         <div>
           <div>
             <DiscountPercentageSpan>{discountPercentage}%</DiscountPercentageSpan>
