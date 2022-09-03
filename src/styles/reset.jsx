db82af419a3215379569e4992e09712311b32a23
jungsoo <sunpl13@naver.com>
2022-09-03 14:26:36 +0900

    [#11] chore : App 스타일링 수정

---
+++ src/styles/reset.jsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+export const AppContainer = styled.div`
+  width: 1440px;
+  height: 100%;
+  border-left: 1px solid #e2e2e2;
+  margin: 0 auto;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu',
+    'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+`;
