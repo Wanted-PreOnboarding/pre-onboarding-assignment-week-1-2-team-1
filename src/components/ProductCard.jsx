97c230435d20c288ceb161e96932ef90a4535050
unsnruu <unsnruu@gmail.com>
2022-09-03 20:22:23 +0900

    [#4] feat : 새로운 스타일 컴포넌트 생성
    가독성을 위해서 새로운 스타일 컴포넌트를 생성했습니다.
    기존 중첩되어 있던 ProductCardContainer에서 분리하였습니다.

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,25 +1,23 @@
 import styled from '@emotion/styled';
 
-//todo 리팩토링 과정을 통해서 style로 컴포넌트 옮기기
-
+//todo: 리팩토링 과정을 통해서 style로 컴포넌트 옮기기
 const ProductCardContainer = styled.div`
   background-color: red;
   width: 240px;
   height: 334px;
   display: flex;
   flex-direction: column;
-
   & img {
     width: 100%;
   }
+`;
+const ProductCardDescription = styled.div`
   & h3 {
     font-size: 14px;
   }
-  .card-description {
-  }
 `;
 
-function ProductCard({ id, img, isSale, name, price, salePrice }) {
+function ProductCard({ id, img, isSale, name, price, salePrice, chip }) {
   const discountPercentage = (100 - (salePrice / price) * 100).toFixed(0);
 
   return (
@@ -28,21 +26,19 @@ function ProductCard({ id, img, isSale, name, price, salePrice }) {
         {/* 이후에 absolute로 sale, best,md badge를 추가하기 위해서 div로 감쌌음*/}
         <img src={img} alt="" />
       </div>
-      <div className="card-description">
+      <ProductCardDescription>
+        <h3>{name}</h3>
         <div>
-          <h3>{name}</h3>
-          <div>
-            {/* discount and sale price */}
-            <span>{discountPercentage}%</span>
-            <span>{salePrice}</span>
-          </div>
-          <div>
-            {/* price and delivery fee */}
-            <span>{price}</span>
-            <span>무료배송</span>
-          </div>
+          {/* discount and sale price */}
+          <span>{discountPercentage}%</span>
+          <span>{salePrice}</span>
         </div>
-      </div>
+        <div>
+          {/* price and delivery fee */}
+          <span>{price}</span>
+          <span>무료배송</span>
+        </div>
+      </ProductCardDescription>
     </ProductCardContainer>
   );
 }
