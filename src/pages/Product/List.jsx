e3410e4c1fecf6fd9563fc3f4c8628901786ebb6
unsnruu <unsnruu@gmail.com>
2022-09-05 01:15:56 +0900

    [#30] fix : pagination 문제 수정
    이슈와 별개로 pagination이 첫번째와 마지막 장에서만 아이콘이 정상 작동하여
    disabled의 조건을 수정했습니다.

--- src/pages/Product/List.jsx
+++ src/pages/Product/List.jsx
@@ -166,9 +166,9 @@ function List() {
         ))}
       </ProductCardContainer>
       <Pagination>
-        <StyledPrev onClick={onClickPrevPage} disabled={onPage < pagination || pagination === 1} />
+        <StyledPrev onClick={onClickPrevPage} disabled={onPage === 1} />
         {pageSpans}
-        <StyledNext onClick={onClickNextPage} disabled={onPage > 1 || pagination === 1} />
+        <StyledNext onClick={onClickNextPage} disabled={onPage >= pagination} />
       </Pagination>
     </ListContainer>
   );
